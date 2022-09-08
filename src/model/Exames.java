a7cc5a35dbc175111af8a6134556d3c4447e39f2
rbglacerdaa <108641503+rbglacerdaa@users.noreply.github.com>
2022-09-08 20:49:03 -0300

    Criado models para agendamentos e seus tipos, mantido funções de teste temporariamente para facilitar entedimento

---
+++ src/model/Exames.java
@@ -0,0 +1,30 @@
+package model;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+
+public class Exames extends Agendamentos{
+    private String nome;
+    private String resultado;
+
+    public Exames(String nome, String resultado, Calendar data, String descricao, ArrayList<String> imagens, ArrayList<String> videos) {
+        super(data, descricao, imagens, videos);
+        this.nome = nome;
+        this.resultado = resultado;
+    }
+
+    //get exame infos
+    public ArrayList<String> getExameInfos(){
+        //retorna um array com as informações do exame
+        ArrayList<String> infos = new ArrayList<String>();
+        infos.add("Nome: " + this.nome);
+        infos.add("Resultado: " + this.resultado);
+        infos.add("Data: " + this.data.getTime());
+        infos.add("Descrição: " + this.descricao);
+        infos.add("Imagens: " + this.imagens);
+        infos.add("Videos: " + this.videos);
+        return infos;
+    }
+
+}
