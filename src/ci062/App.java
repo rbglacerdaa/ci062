8fcdd50b1bee2d42b399bb416ea9d41530c1b1ab
ricardofariasg4 <112911206+ricardofariasg4@users.noreply.github.com>
2022-09-15 23:35:18 -0300

    Commit novo

--- src/ci062/App.java
+++ src/ci062/App.java
@@ -4,15 +4,24 @@ import controller.*;
 import model.*;
 import viewText.TelaCadastro;
 import java.lang.*;
-public class App {
+
+public class App 
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("App iniciado");
 		Usuario usuario = new Usuario();
+		
+		String nome = "";
+		
 		GerenciadorConsultas gConsultas = new GerenciadorConsultas();
-		gConsultas.CriaConsulta(novaConsulta, usuario);
-		GerenciadorConsultas.AdicionaDadoConsulta("medico:","luis",usuario);
+		//gConsultas.CriaConsulta(novaConsulta, usuario);
+		//GerenciadorConsultas.AdicionaDadoConsulta("medico:","luis",usuario);
+		
+		nome = GerenciadorUsuario.retornaDado(1);
+		
+		System.out.println("Dado retornado: ");
 		
 	
 		
